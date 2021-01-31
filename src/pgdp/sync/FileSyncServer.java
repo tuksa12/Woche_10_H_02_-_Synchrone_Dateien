e3f0094d0b12c8cf3416ee737cd8d16998c0e37d
DESKTOP-2KIND8H\Raphael Tuksa <rapha.tuksa@hotmail.com>
2021-01-31 17:01:01 +0100

    Final

--- src/pgdp/sync/FileSyncServer.java
+++ src/pgdp/sync/FileSyncServer.java
@@ -11,14 +11,14 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
+//The server will not work when the updateIfNewer is called, but the the idea should be more or less right
 public class FileSyncServer extends FileSyncParticipant{
-    // Server commands
+    // Status responses
     static final String UPDATE = "UPDATE";
     static final String GET = "GET";
     static final String LIST = "LIST";
 
-    // Status responses
+    // Client responses
     static final String SUCCESS = "SUCCESS";
     static final String FAILED = "FAILED";
 
@@ -32,7 +32,7 @@ public class FileSyncServer extends FileSyncParticipant{
     }
 
     @Override
-    public void run() {
+    public void run() {//The same as in the client, with the addition of the helpMethod initiate()
         try (ServerSocket server = new ServerSocket(port)) {
             // Pass UTF-8 for both IO streams as the charset we want to use
             in = new BufferedReader(new InputStreamReader(server.accept().getInputStream(), StandardCharsets.UTF_8));
@@ -46,7 +46,7 @@ public class FileSyncServer extends FileSyncParticipant{
             System.err.println("Client connection failed: " + e);
         }
     }
-
+    //Initiates the "conversation" by printing "LIST"
     private void initiate(){
         out.println(LIST);
     }
@@ -55,65 +55,64 @@ public class FileSyncServer extends FileSyncParticipant{
         String command = in.readLine();
         switch (command) {
             case SUCCESS -> getFileListFromClient();
-            default -> System.err.print("Unknown command from client: " + command);
+            default -> System.err.print("Unknown command from client: " + command);//Case Failed
         }
     }
 
-    private void getFileListFromClient() {
-        ArrayList<String> uri = new ArrayList<>();
-        ArrayList<Instant> lastModifiedTime = new ArrayList<>();
+    private void getFileListFromClient() {//Get the information from the client
+        ArrayList<String> uri = new ArrayList<>();//ArrayList of URI from the user
+        ArrayList<Instant> lastModifiedTime = new ArrayList<>();//ArrayList of lastModifiedTime from the user
         try {
-            Map<String, Instant> filesFromServer = folderSynchronizer().scan();
-            int filesNumber= in.read();
-            for (int i = 0; i < filesNumber; i++) {
+            Map<String, Instant> filesFromServer = folderSynchronizer().scan();//Files from the server
+            int filesNumber= in.read();//Number of files that the client has
+            for (int i = 0; i < filesNumber; i++) {//Loop to receive the input from the client and store in the two ArrayLists. It will repeat for the number of "filesNumber"
                 uri.add(in.readLine());
                 lastModifiedTime.add(Instant.parse(in.readLine()));
 
-            } if(uri.size() > filesFromServer.size()){
+            } if(uri.size() > filesFromServer.size()){//If client has more files -> GET
                 out.println(GET);
                 for (int i = 0; i < uri.size(); i++) {
-                    if(!filesFromServer.keySet().contains(uri.get(i))){
+                    if(!filesFromServer.keySet().contains(uri.get(i))){//If the server doesn't have the file, get the file
                         out.println(uri.get(i));
                         updateFileFromServer(uri.get(i));
-                    } else if(filesFromServer.get(uri.get(i)).isBefore(lastModifiedTime.get(i))){
+                    } else if(filesFromServer.get(uri.get(i)).isBefore(lastModifiedTime.get(i))){//if the server has the file, but not the most recent version
                         out.println(uri.get(i));
                         updateFileFromServer(uri.get(i));
                     }
                 }
-            } else if(uri.size() < filesFromServer.size()){
+            } else if(uri.size() < filesFromServer.size()){//If server has more files -> UPDATE
                 List<String> keys = new ArrayList<>(filesFromServer.keySet());
                 out.println(UPDATE);
                 for (int i = 0; i < filesFromServer.size(); i++) {
-                    if(!uri.contains(keys.get(i))){
+                    if(!uri.contains(keys.get(i))){//If the client doesn't have the file, send the file
                         out.println(keys.get(i));
                         sendFileContentToClient(keys.get(i));
-                    } else if(filesFromServer.get(uri.get(i)).isAfter(lastModifiedTime.get(i))){
+                    } else if(filesFromServer.get(uri.get(i)).isAfter(lastModifiedTime.get(i))){//if the client has the file, but not the most recent version
                         out.println(uri.get(i) + "\n" + uri.get(i) + " wurde auf Server editiert");
                         sendFileContentToClient(uri.get(i));
                     }
                 }
-            } else {
-                List<String> keys = new ArrayList<>(filesFromServer.keySet());
+            } else {//If the sizes of files are equal
                 for (int i = 0; i < filesFromServer.size(); i++) {
-                    if(filesFromServer.get(uri.get(i)).isBefore(lastModifiedTime.get(i))){
+                    if(filesFromServer.get(uri.get(i)).isBefore(lastModifiedTime.get(i))){//if the server has the file, but not the most recent version
                         out.println(GET);
                         out.println(uri.get(i));
                         updateFileFromServer(uri.get(i));
-                    }else if (filesFromServer.get(uri.get(i)).isAfter(lastModifiedTime.get(i))) {
+                    }else if (filesFromServer.get(uri.get(i)).isAfter(lastModifiedTime.get(i))) {//if the client has the file, but not the most recent version
                         out.println(UPDATE);
                         out.println(uri.get(i) + "\n" + uri.get(i) + " wurde auf Server editiert");
                         sendFileContentToClient(uri.get(i));
                     }
                 }
             }
-            FileSyncUtil.sleepFiveSeconds();
+            FileSyncUtil.sleepFiveSeconds();//Step 10
             initiate();
         } catch (IOException e) {
-            System.err.println("Not a valid file number.");
+            System.err.println("Error while working with the files.");
         }
     }
 
-    private void sendFileContentToClient(String file) throws IOException {
+    private void sendFileContentToClient(String file) throws IOException {//Same idea as the method in the client, bt with a parameter
         FileContent fileContent;
         try {
             fileContent = folderSynchronizer().getFileContent(file);
@@ -125,13 +124,13 @@ public class FileSyncServer extends FileSyncParticipant{
         sendFileContent(fileContent);
     }
 
-    private void updateFileFromServer(String file) throws IOException {
+    private void updateFileFromServer(String file) throws IOException {//Same idea as the method in the client, but with a parameter and the if statement
         if(in.readLine().equals(SUCCESS)){
             receiveAndUpdateFile(file);
         }
     }
 
-    void receiveAndUpdateFile(String file) throws IOException {
+    void receiveAndUpdateFile(String file) throws IOException {//Same as in client
         Instant lastModTime = Instant.parse(in.readLine());
         int lineCount = Integer.parseInt(in.readLine());
         List<String> lines = new ArrayList<>();
@@ -146,7 +145,7 @@ public class FileSyncServer extends FileSyncParticipant{
         }
     }
 
-    void sendFileContent(FileContent fileContent) {
+    void sendFileContent(FileContent fileContent) {//Same as in client
         out.println(fileContent.getLastModifiedTime());
         out.println(fileContent.getLines().size());
         for (String line : fileContent.getLines()) {
@@ -154,7 +153,7 @@ public class FileSyncServer extends FileSyncParticipant{
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {//Main method but with only two parameters
         if (args.length != 2)
             throw new IllegalArgumentException("Invalid number of arguments");
         Path folder = Path.of(args[0]);
--- src/pgdp/sync/FolderSynchronizer.java
+++ src/pgdp/sync/FolderSynchronizer.java
@@ -15,116 +15,74 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class FolderSynchronizer {
-	Path folder;
+	final Path folder;
 
+	//Constructor
 	public FolderSynchronizer(Path folder) {
 		this.folder = folder;
 	}
 
 	public Map<String, Instant> scan() throws IOException {
 		Map<String, Instant > result = new HashMap<>();
-		Files.walk(folder)
+		Files.walk(folder)//Walk through the folder
 				.forEach(file -> {
 					try {
-						if(Files.isRegularFile(file)){
+						if(Files.isRegularFile(file)){//If it is a regularFile, add the file to the map
 							result.put(FileSyncUtil.pathToRelativeUri(folder,file),Files.getLastModifiedTime(file).toInstant());
 						}
-					} catch (IOException ignored) {
+					} catch (IOException ignored) {//Ignores the error, to continue the "walk"
 					}
 				});
 		return result;
 	}
 
+	//I couldn't manage to make updateIfNewer work, but the idea behind should be more or less in the right direction
 	public boolean updateIfNewer(String fileInFolderRelativeUri, FileContent fileContent) throws IOException {
+		//The next two if statements I did to try to fix the artemis test error
 		if(fileInFolderRelativeUri.equals("") || fileContent == null){
 			return false;
 		}
-//		if(!Files.exists(folder)){
-//			 Path k = Files.createDirectories(folder);
-//		}
+		if(!Files.exists(folder)){
+			 Files.createDirectories(folder);
+		}
 
-		boolean containsFile = Files.walk(folder)
+		boolean containsFile = Files.walk(folder)//Tries to find the file in the folder
 				.anyMatch(file -> FileSyncUtil.pathToRelativeUri(folder,file).equals(fileInFolderRelativeUri));
 
 		if(containsFile){
 			Path file = FileSyncUtil.relativeUriToPath(folder, fileInFolderRelativeUri);
-			if (Files.getLastModifiedTime(file).toInstant().isBefore(fileContent.getLastModifiedTime())){
+			if (Files.getLastModifiedTime(file).toInstant().isBefore(fileContent.getLastModifiedTime())){//Compares the lastModifiedTime of both files
 					BufferedWriter writer = new BufferedWriter(new FileWriter(file.toString()));
 					writer.write(fileContent.getLines().toString());
 					Files.setLastModifiedTime(file,FileTime.from(fileContent.getLastModifiedTime()));
 			}
-		} else{
+		} else{//Creates a new file because the folder doesn't have the file
 			Path newFile = Files.createDirectories(folder);
 			BufferedWriter writer = new BufferedWriter(new FileWriter(newFile.toString()));
 			writer.write(fileContent.getLines().toString());
 			Files.setLastModifiedTime(newFile,FileTime.from(fileContent.getLastModifiedTime()));
 		}
 
-		return Files.walk(folder)
+		return Files.walk(folder)//Returns the if the file was add to the folder. With the method working, should return true
 				.anyMatch(file -> FileSyncUtil.pathToRelativeUri(folder,file).equals(fileInFolderRelativeUri));
 	}
 
 
-//	public boolean updateIfNewer(String fileInFolderRelativeUri, FileContent fileContent) throws IOException {
-//		Path filePath = FileSyncUtil.relativeUriToPath(folder,fileInFolderRelativeUri);
-//		boolean contains = Files.walk(folder)
-//				.anyMatch(file -> file.getFileName().equals(filePath));
-//		if (!contains){
-//			//Files.createDirectories(folder,Files.getAttribute(filePath,fileContent.getLines().toString()));
-//		} else {
-//			Files.walk(folder)
-//			.filter(file -> file.getFileName().equals(filePath))
-//			.map(file -> {
-//				try {
-//					if(Files.getLastModifiedTime(file).toInstant().isBefore(fileContent.getLastModifiedTime())){
-//						Files.setAttribute(file,fileContent.getLines().toString(),fileContent.getLastModifiedTime());
-//						Files.setLastModifiedTime(file, FileTime.from(fileContent.getLastModifiedTime()));
-//					}
-//				} catch (IOException e) {
-//				}
-//			return true;});
-//		}
-////		Files.walk(folder)
-////				.filter(file -> file.equals(path))
-////				.forEach(file -> {
-////					try {
-////						if(Files.getLastModifiedTime(file).toInstant().isBefore(fileContent.getLastModifiedTime())){
-////							Files.cr
-////							Files.setLastModifiedTime(file, FileTime.from(fileContent.getLastModifiedTime()));
-////						}
-////					} catch (IOException e) {
-////					}
-////				});
-//		return true;
-//	}
 	public FileContent getFileContent(String fileInFolderRelativeUri) throws IOException {
 		List<FileContent> result = Files.walk(folder)
-				.filter(file -> FileSyncUtil.pathToRelativeUri(folder,file).equals(fileInFolderRelativeUri))
+				.filter(file -> FileSyncUtil.pathToRelativeUri(folder,file).equals(fileInFolderRelativeUri))//Filter to find the specific file in the folder
 				.map(file -> {
 					try {
 						 List<String> content = Files.readAllLines(file);
 						 Instant lastModifiedDate = Files.getLastModifiedTime(file).toInstant();
-						 return new FileContent(lastModifiedDate,content);
+						 return new FileContent(lastModifiedDate,content);//Returns the file content of the specific file
 					} catch (IOException e) {
-						return new FileContent(null,null);//Mudar isso
+						return new FileContent(null,null);//If there is a error, it will return an empty FileContent
 					}
 				})
 				.collect(Collectors.toList());
-		return result.get(0);
+
+		return result.get(0);//Just a easy way to pick the first and only element present in the list
 	}
 
-//	public FileContent getFileContent(String fileInFolderRelativeUri) throws IOException {
-//		Path path = FileSyncUtil.relativeUriToPath(folder,fileInFolderRelativeUri);
-//		List<String> content = new ArrayList<>();
-//		Files.walk(folder)
-//			.forEach(file -> {
-//					content.add(file.toFile().toString());//Antes estava file.getFileName().toString(),testar se funciona
-//			});
-//		Instant instant = null;
-//		try{
-//			instant = Files.getLastModifiedTime(path).toInstant();
-//		} catch (IOException ignored) {
-//		}
-//		return new FileContent(instant,content);
-//	}
 }
